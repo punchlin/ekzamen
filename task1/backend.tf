7478062674561cf3091289a819cc8a8df7731037
yulianna <yulianna.duplina.pp.2022@lpnu.ua>
2026-04-07 00:00:09 +0300

    Fix backend.tf: add skip flags and https

--- task1/backend.tf
+++ task1/backend.tf
@@ -1,10 +1,14 @@
 terraform {
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1" # Залишаємо так для сумісності
-    bucket                      = "duplina-terraform-state" # ТВОЯ НАЗВА БАКЕТА
+    endpoint                    = "https://fra1.digitaloceanspaces.com" 
+    region                      = "us-east-1" # Це залишаємо як є
+    bucket                      = "punchlin-terraform-state" # ПЕРЕВІР, ЧИ ТАКА НАЗВА В ТЕБЕ В DO!
     key                         = "terraform.tfstate"
+    
+    # Ці три рядки критично важливі для DigitalOcean:
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true # ЦЕ ВИПРАВИТЬ ГОЛОВНУ ПОМИЛКУ (STS/IAM)
   }
 }
